bac1e6083002f9d83ad88c35d89cc633775be3df
Denis Kalagin <deniskalagin@gmail.com>
2023-07-01 13:24:37 +0600

    Add brain-prime.js and game-prime.js

---
+++ src/games/game-prime.js
@@ -0,0 +1,17 @@
+import { makeRandomNumber } from '../utils.js';
+import makeGame from '../index.js';
+
+const greetings = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const primeNumber = () => {
+  const questionNumberumber = makeRandomNumber(0, 100);
+  const correctAnsweranswer = questionNumberumber % questionNumberumber === 1 ? 'yes' : 'no';
+
+  return [questionNumberumber, correctAnsweranswer];
+};
+
+const gamePrime = () => {
+  makeGame(greetings, primeNumber);
+};
+
+export default gamePrime;
