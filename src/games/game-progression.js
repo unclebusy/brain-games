207c08dec081956bc3470f680d427ddad6d45ca4
Denis Kalagin <deniskalagin@gmail.com>
2023-07-06 19:38:01 +0600

    Update game-progression.js

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -3,32 +3,40 @@ import generateRandomNumber from '../utils.js';
 
 const greetings = 'What number is missing in the progression?';
 
-const makeProgression = () => {
+const isQuestion = (number, length, step, miss) => {
+  const arr = [number];
+
+  for (let i = 1; i < length; i += 1) {
+    arr.push(arr[arr.length - 1] + step);
+  }
+  arr[miss] = '..';
+
+  return arr.join(' ');
+};
+
+const isAnswer = (number, step, miss) => {
+  let answer = number;
+
+  for (let i = 0; i < miss; i += 1) {
+    answer += step;
+  }
+
+  return String(answer);
+};
+
+const generationProgression = () => {
   const arrLength = 10;
-  const missNumberPosinion = generateRandomNumber(0, 9);
+  const missPosinion = generateRandomNumber(0, 9);
   const stepProgression = generateRandomNumber(1, 10);
   const firtNumberProgression = generateRandomNumber(0, 100);
-  let question = [firtNumberProgression];
-  let correctAnswer = firtNumberProgression;
-
-  for (let i = 1; i < arrLength; i += 1) {
-    question.push(question[question.length - 1] + stepProgression);
-  }
-
-  question[missNumberPosinion] = '..';
-
-  for (let i = 0; i < missNumberPosinion; i += 1) {
-    correctAnswer += stepProgression;
-  }
-
-  question = question.join(' ');
-  correctAnswer = String(correctAnswer);
+  const question = isQuestion(firtNumberProgression, arrLength, stepProgression, missPosinion);
+  const correctAnswer = isAnswer(firtNumberProgression, stepProgression, missPosinion);
 
   return [question, correctAnswer];
 };
 
 const gameProgression = () => {
-  startGame(greetings, makeProgression);
+  startGame(greetings, generationProgression);
 };
 
 export default gameProgression;
