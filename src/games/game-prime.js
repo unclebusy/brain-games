fba1a6345a20281ffeacb3ed5e9b4f1e764decc8
Denis Kalagin <deniskalagin@gmail.com>
2023-07-06 18:46:57 +0600

    Update game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -3,23 +3,22 @@ import startGame from '../index.js';
 
 const greetings = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const isPrimeNymber = (number) => {
+  if (number === 1 || number === 2) {
+    return 'yes';
+  }
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return 'no';
+    }
+  }
+
+  return 'yes';
+};
+
 const primeNumber = () => {
   const randomNumber = generateRandomNumber(0, 100);
-
-  const isPrimeNymber = () => {
-    if (randomNumber === 1 || randomNumber === 2) {
-      return 'yes';
-    }
-    for (let i = 2; i < randomNumber; i += 1) {
-      if (randomNumber % i === 0) {
-        return 'no';
-      }
-    }
-
-    return 'yes';
-  };
-
-  const correctAnswer = isPrimeNymber();
+  const correctAnswer = isPrimeNymber(randomNumber);
 
   return [randomNumber, correctAnswer];
 };
