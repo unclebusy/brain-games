dad243778db1c64d94586fc59ec9634c6fd0a092
Denis Kalagin <deniskalagin@gmail.com>
2023-07-08 13:21:41 +0600

    Update game-even.js

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -5,15 +5,16 @@ const greetings = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => {
   if (number % 2 === 0) {
-    return 'yes';
+    return true;
   }
 
-  return 'no';
+  return false;
 };
 
 const generateRoundData = () => {
   const questionNumber = generateRandomNumber(0, 100);
-  const correctAnswer = isEven(questionNumber);
+  const correctAnswer = isEven(questionNumber) ? 'yes' : 'no';
+
   return [questionNumber, correctAnswer];
 };
 
