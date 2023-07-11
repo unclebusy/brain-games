84ca00de2b52558aac0a5c2f70d38c684c83cf42
Denis Kalagin <deniskalagin@gmail.com>
2023-07-11 11:46:12 +0600

    Update game-prime.js

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -4,13 +4,13 @@ import startGame from '../index.js';
 const greetings = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  if (number === 1 || number === 2) {
+  if (number < 2) {
     return true;
   }
   const sqrtNumber = Math.floor(Math.sqrt(number));
 
   for (let i = 2; i <= sqrtNumber; i += 1) {
-    if (number < 2) {
+    if (number % i === 0) {
       return false;
     }
   }
